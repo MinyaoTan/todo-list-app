7f9fa720fe0832c3d50eb168f2ab6d9d8bcabf9b
Minyao Tan <mtan3@uw.edu>
2018-11-25 16:20:14 -0800

    enable functionality to add items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 // Check off specific todos by clicking
-$("li").on("click", function() {
+$("ul").on("click", "li",  function() {
 	$(this).toggleClass("completed");
 })
 
 // Click on X to delete todo
-$("span").on("click", function(event) {
+$("ul").on("click", "span", function(event) {
 	event.stopPropagation();
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
+})
+
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 })
