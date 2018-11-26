07356293baf870181d9a23cae9a7f97bd6cee658
Minyao Tan <mtan3@uw.edu>
2018-11-25 16:05:48 -0800

    enable functionality to delete items

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 $("li").on("click", function() {
 	$(this).toggleClass("completed");
 })
+
+// Click on X to delete todo
+$("span").on("click", function(event) {
+	event.stopPropagation();
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+})
