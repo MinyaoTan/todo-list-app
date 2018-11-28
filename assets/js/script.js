01a7be0660aa46fd04462814ad40671ea276e588
Minyao Tan <mtan3@uw.edu>
2018-11-27 21:18:11 -0800

    added logic to toggle plus/minus icon animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Check off specific todos by clicking
 $("ul").on("click", "li",  function() {
 	$(this).toggleClass("completed");
-})
+});
 
 // Click on X to delete todo
 $("ul").on("click", "span", function(event) {
@@ -9,7 +9,7 @@ $("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
-})
+});
 
 $("input[type='text']").keypress(function(event) {
 	if (event.which === 13) {
@@ -17,4 +17,29 @@ $("input[type='text']").keypress(function(event) {
 		$(this).val("");
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 	}
-})
+});
+
+$(".fa-minus").click(function() {
+	$("input[type='text']").slideToggle(300);
+	$(this).toggleClass("hide");
+	$(".fa-plus").toggleClass("hide");
+
+	$(".fa-plus").fadeIn(0, function() {
+		$(".fa-plus").toggleClass("fa-rotate");
+	});
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text']").slideToggle(300);
+	$(this).toggleClass("fa-rotate");
+
+	$(this).fadeOut(300, function() {
+		$(this).toggleClass("hide");
+		$(".fa-minus").toggleClass("hide");
+		$(".fa-minus").css("display", "block");
+	});
+});
+
+if ($(".fa-plus").hasClass("fa-rotate")) {
+	$(".fa-plus").toggleClass("fa-rotate");
+}
